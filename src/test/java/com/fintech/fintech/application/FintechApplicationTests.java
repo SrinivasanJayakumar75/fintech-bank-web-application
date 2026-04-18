202faaa9dd5aabee45fd37a78b364546accf4d52
SrinivasanJayakumar75 <sjayakumar862412@gmail.com>
2026-04-19 00:10:30 +0530

    connected to database

---
+++ src/test/java/com/fintech/fintech/application/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.fintech.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
